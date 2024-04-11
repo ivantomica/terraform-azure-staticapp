558d65d94d7e9f0aebe3189351516b87b629c65d
Ivan Tomica <ivan@tomica.net>
2024-04-11 13:41:40 +0200

    Remove migrations file

--- migrations.tf
+++
@@ -1,9 +0,0 @@
-moved {
-  from = azurerm_static_site.site
-  to   = azurerm_static_web_app.site
-}
-
-moved {
-  from = azurerm_static_site_custom_domain.domain
-  to   = azurerm_static_web_app_custom_domain.domain
-}
