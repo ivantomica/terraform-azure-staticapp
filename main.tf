55f362352d737ee1c7f7dec39d954735367db375
Ivan Tomica <ivan@tomica.net>
2024-04-11 12:28:17 +0200

    Upgrade module to use web_app

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_static_site" "site" {
+resource "azurerm_static_web_app" "site" {
   name                = var.name
   resource_group_name = var.resource_group_name
   location            = var.location
@@ -22,7 +22,7 @@ resource "azurerm_static_site" "site" {
 resource "azurerm_static_site_custom_domain" "domain" {
   for_each = var.custom_domains
 
-  domain_name     = each.key
-  static_site_id  = azurerm_static_site.site.id
-  validation_type = each.value
+  domain_name       = each.key
+  static_web_app_id = azurerm_static_web_app.site.id
+  validation_type   = each.value
 }
